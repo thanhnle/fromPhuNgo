83cd6d946d078b36d6ec1f8a653da09926b9105e
phuhuyngo <phuhuyngo@C02DV9Y1ML7H.local>
2022-02-11 10:26:17 +0700

    change environment to staging

--- Test Suites/API/mc.ts
+++ Test Suites/API/mc.ts
@@ -24,5 +24,14 @@
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/API/ma</testSuiteEntity>
       </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <runConfigurationId>Chrome (headless)</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/API/ma</testSuiteEntity>
+      </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
